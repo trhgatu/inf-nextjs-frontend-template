b07e3660aed9e05100805b4500e8b2a37e5f3478
Tran Hoang Anh Tu <trananhtu1112003@gmail.com>
2025-06-25 09:40:47 +0700

    fix(format): format code

--- middleware.ts
+++ middleware.ts
@@ -1,19 +1,19 @@
 // middleware.ts
-import { NextResponse } from 'next/server'
-import type { NextRequest } from 'next/server'
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
 
 export function middleware(request: NextRequest) {
-  const { pathname } = request.nextUrl
-  const refreshToken = request.cookies.get('refreshToken')?.value
+  const { pathname } = request.nextUrl;
+  const refreshToken = request.cookies.get('refreshToken')?.value;
 
   if (pathname === '/login' && refreshToken) {
-    const homeUrl = new URL('/', request.url)
-    return NextResponse.redirect(homeUrl)
+    const homeUrl = new URL('/', request.url);
+    return NextResponse.redirect(homeUrl);
   }
 
-  return NextResponse.next()
+  return NextResponse.next();
 }
 
 export const config = {
   matcher: ['/login'],
-}
+};
